27f4f8f6057e0e55cabd49a7abaf1073f0dbbde6
Itamar Junior <itamar.junior@irriga.net>
2025-12-08 17:05:43 -0300

    feat: criar servidor

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,10 @@
-console.log("olá amigo");
+import * as http from 'http';
+
+const server = http.createServer((request: http.IncomingMessage, response: http.ServerResponse) => {}
+);
+
+const PORT = 3333;
+
+server.listen(process.env.PORT, () => {
+  console.log(`Servidor Iniciado na Porta ${PORT}`);
+});
